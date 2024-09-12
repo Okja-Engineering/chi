a860d6383813a8f38c76efe7bfd98de477f544d6
mattmark <imagineux@gmail.com>
2024-09-12 11:16:13 -0500

    Package: Primitives Base Setup

---
+++ .changeset/grumpy-turtles-brake.md
@@ -0,0 +1,7 @@
+---
+"@okja/chi-colors": patch
+"@okja/chi-icons": patch
+"@okja/figma-cli": patch
+---
+
+Publish Beta
---
+++ packages/primitives/eslint.config.js
@@ -0,0 +1,9 @@
+import baseConfig from "@okja/eslint-config/base";
+
+/** @type {import('typescript-eslint').Config} */
+export default [
+  {
+    ignores: [],
+  },
+  ...baseConfig,
+];
